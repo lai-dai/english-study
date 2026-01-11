fef33a76d12ee56ae0877ec44e11d0b099d3066b
laidai <laidai9966@gmail.com>
2026-01-11 23:19:04 +0700

    up github page

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,17 @@
-import type { NextConfig } from "next"
 import createMDX from "@next/mdx"
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
   pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  output: "export", // Bắt buộc để deploy lên GitHub Pages
+  // Optional: Add a trailing slash to generate folder structures
+  // (e.g., /about becomes /about/index.html instead of /about.html)
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // GitHub Pages không hỗ trợ Image Optimization mặc định của Next.js
+  },
+  basePath: "/lai-dai/english-study",
 }
 
 const withMDX = createMDX({
